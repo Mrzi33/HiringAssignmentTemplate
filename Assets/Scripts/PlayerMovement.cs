b2c2903edaa567ed0441afe86a3945c28b465cbe
Pavel Mrazek <paulfreezer@seznam.cz>
2023-11-17 20:45:47 +0100

    Reworked player group movement behaviour, now works with joints

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,32 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+
+
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField]
+    float forwardSpeed = 5f;
+    [SerializeField]
+    float sideSpeed = 5f;
+
+    [SerializeField]
+    float leftSideLimit, rightSideLimit;
 
 
     private void Update()
     {
         Vector3 movement = Vector3.zero;
+        movement.x += forwardSpeed;
         if (Input.GetKey(KeyCode.A))
         {
-            movement += new Vector3(0, 0, 5);
+            movement += new Vector3(0, 0, 5) * sideSpeed;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            movement += new Vector3(0, 0, -5);
+            movement += new Vector3(0, 0, -5) * sideSpeed;
         }
-        if (Input.GetKey(KeyCode.W))
-        {
-            movement += new Vector3(5, 0, 0);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            movement += new Vector3(-5, 0, 0);
-        }
-        
+
         transform.position += movement * Time.deltaTime;
-        transform.position = new Vector3(transform.position.x,transform.position.y, Mathf.Clamp(transform.position.z, -4.5f, 4.5f));
+        transform.position = new Vector3(transform.position.x,transform.position.y, Mathf.Clamp(transform.position.z, leftSideLimit, rightSideLimit));
 
     }
 
